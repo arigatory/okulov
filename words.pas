7bd228d001d93dd803ec9b14c2afe303b572fae6
Ivan Ubuntu20 <arigatory@gmail.com>
2021-07-04 01:21:51 -0700

    first commit

---
+++ words.pas
@@ -0,0 +1,35 @@
+program words;
+const n=20; m=10; {n - number of words, m = number of letters}
+
+type TString=string[m];
+
+var A:array[1..n] of TString;
+	s:string;
+	k, i: integer;
+
+procedure RemoveSpaces(var s:string);
+  begin
+	while (s<>'') and (s[1] = ' ') do delete(s,1,1);
+  end;
+
+function GetWord(var s:string):TString;
+  begin
+	GetWord:=copy(s,1,pos(' ',s)-1);
+	delete(s,1,pos(' ',s));
+	RemoveSpaces(s);
+  end;
+
+begin
+	writeln('Введите текст');
+	readln(s);
+	s:=s+' ';
+	RemoveSpaces(s);
+	k:=0;
+	while s <> '' do begin
+		k:=k+1;
+		A[k] := GetWord(s);
+	end;
+
+	for i:=1 to k do writeln(A[i]);
+	readln;
+end.
